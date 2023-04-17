11f99e390a20d724b26f854a118541ba12d4036b
Ahinojosa13 <128249514+Ahinojosa13@users.noreply.github.com>
2023-04-17 12:19:56 -0400

    Added Folders for GI

---
+++ Easy/e.js
@@ -0,0 +1,3 @@
+// Create two variables and assign a person’s name to each. Console log a statement that says which name is shorter or longer, and by how many characters.  
+// Example output: “The name Thomas is longer than John by 2 characters”
+
---
+++ Very Easy/ve.js
@@ -0,0 +1,1 @@
+// Create two variables and assign a number to each. Console log the difference between the numbers. Example output: “The difference between 6 and 2 is 4”
