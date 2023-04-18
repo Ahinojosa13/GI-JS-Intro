5a65a3c00bc28b2cbcfcbc9b1bda4f1fb295faa8
Ahinojosa13 <128249514+Ahinojosa13@users.noreply.github.com>
2023-04-18 09:48:16 -0400

    new files

---
+++ Medium/m.js
@@ -0,0 +1,2 @@
+//  Write a program to tell if someone is shouting (typing in all caps), whispering (typing in all lowercase), or neither. 
+// Use prompt to get user input, and then console log whether the user was shouting, whispering, or talking normally.
--- Very Easy/ve.js
+++ Very Easy/ve.js
@@ -1,1 +1,9 @@
 // Create two variables and assign a number to each. Console log the difference between the numbers. Example output: “The difference between 6 and 2 is 4”
+var number1=21;
+var number2=13;
+
+
+ document.write("Difference = "+difference(num1,num2));
+
+
+cosole.log 
