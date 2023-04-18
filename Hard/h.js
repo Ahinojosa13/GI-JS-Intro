eb8a4f058ffdea2a4cc8879eefed2b0761e651d2
Ahinojosa13 <128249514+Ahinojosa13@users.noreply.github.com>
2023-04-18 11:25:15 -0400

    Finished Hard

--- Hard/h.js
+++ Hard/h.js
@@ -1,1 +1,15 @@
 // Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
+let num1 = 20;
+let num2 = 2;
+//Add
+let add = num1 + num2;
+console.log("Sum", add);
+//Subtract
+let difference = num1 - num2;
+console.log("Difference", difference);
+//Multiply
+let multiply = num1 * num2;
+console.log("multiply", multiply)
+//Divide
+let divide = num1 / num2;
+console.log("divide", divide)
--- Very Easy/ve.js
+++ Very Easy/ve.js
@@ -1,9 +1,6 @@
 // Create two variables and assign a number to each. Console log the difference between the numbers. Example output: “The difference between 6 and 2 is 4”
-var number1=21;
-var number2=13;
+let num1 = 21;
+let num2 = 13;
 
-
- document.write("Difference = "+difference(num1,num2));
-
-
-cosole.log 
+let sum = num1-num2;
+console.log("Sum:", sum); 
