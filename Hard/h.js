8124c647271099bc72d72dbe6ed997dbe7b60fbe
Ahinojosa13 <128249514+Ahinojosa13@users.noreply.github.com>
2023-04-21 09:28:08 -0400

    n

--- Hard/h.js
+++ Hard/h.js
@@ -1,15 +1,25 @@
 // Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
-let num1 = 20;
-let num2 = 2;
-//Add
-let add = num1 + num2;
-console.log("Sum", add);
-//Subtract
-let difference = num1 - num2;
-console.log("Difference", difference);
-//Multiply
-let multiply = num1 * num2;
-console.log("multiply", multiply)
-//Divide
-let divide = num1 / num2;
-console.log("divide", divide)
+function add(x , y ) {
+    console.log(x+y);
+}
+
+add(2,2);
+
+function subtract(x , y ) {
+    console.log(x-y);
+}
+
+subtract(8,4);
+
+
+function multiply(x , y ) {
+    console.log(x*y);
+}
+
+multiply(2,2);
+
+function divide(x , y ) {
+    console.log(x/y);
+}
+
+divide(8,2);
